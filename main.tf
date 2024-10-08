7cba0b771d3528c3b7639351f7671ce8a9bcb1c8
Tarzone <walekunle@gmail.com>
2024-10-07 23:00:57 -0600

    configure network settings

--- main.tf
+++ main.tf
@@ -1,27 +1,101 @@
-provider "aws" {}
-
-variable cidr_blocks {
-  description = "cidr blocks and name tags for vpc and subnets"
-  type = list(object({
-    cidr_block = string
-    name = string
-  })) 
+provider "aws" {
+  region = "us-east-1"
 }
 
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
 variable avail_zone {}
+variable env_prefix {}
+variable my_ip {}
 
 resource "aws_vpc" "myapp-vpc" {
-  cidr_block = var.cidr_blocks[0].cidr_block
+  cidr_block = var.vpc_cidr_block
   tags = {
-    Name: var.cidr_blocks[0].name
+    Name: "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
   vpc_id = aws_vpc.myapp-vpc.id
-  cidr_block = var.cidr_blocks[1].cidr_block
+  cidr_block = var.subnet_cidr_block 
   availability_zone = var.avail_zone
   tags = {
-    Name: var.cidr_blocks[1].name
+    Name: "${var.env_prefix}-subnet-1"
   }
+}
+
+# Creates a route table (non-default) with igw
+
+# resource "aws_route_table" "myapp-route_table" {
+#   vpc_id = aws_vpc.myapp-vpc.id
+
+#   route {
+#     cidr_block = "0.0.0.0/0"
+#     gateway_id = aws_internet_gateway.myapp-igw.id
+#   }
+#   tags = {
+#     Name: "${var.env_prefix}-rtb"
+#   }
+# }
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  tags = {
+    Name: "${var.env_prefix}-igw"
+  }
+}
+
+# Associates a route table (non-default) to a subnet
+
+# resource "aws_route_table_association" "a-rtb-subnet" {
+#   subnet_id = aws_subnet.myapp-subnet-1.id
+#   route_table_id = aws_route_table.myapp-route_table.id
+# }
+
+resource "aws_default_route_table" "main-rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+  tags = {
+    Name: "${var.env_prefix}-main-rtb"
+  }
+}
+
+resource "aws_default_security_group" "default-sg" {
+
+# # create a security group
+# resource "aws_security_group" "myapp-sg" {
+#   name = "myapp-sg"
+
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "TCP"
+    cidr_blocks = [var.my_ip]
+  }
+  
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name: "${var.env_prefix}-default-sg"
+    # Name: "${var.env_prefix}-sg" # sg tag for created sg
+  }  
 }
