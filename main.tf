39b899565e65167bc3f8023109f45a4ad7f92df9
Tarzone <walekunle@gmail.com>
2024-10-08 04:50:36 -0600

    Adding user data entry commands

--- main.tf
+++ main.tf
@@ -148,6 +148,21 @@ resource "aws_instance" "myapp-server" {
   associate_public_ip_address = true
   key_name = aws_key_pair.ssh-key.key_name
 
+  /* # Manually pass user data
+  user_data = <<EOF 
+                #!/bin/bash
+                sudo yum update -y && sudo yum install docker -y
+                sudo systemctl start docker
+                sudo usermod -aG docker ec2-user
+                docker run -p 8080:80 nginx
+              EOF
+  */
+
+  # Input path for user data
+  user_data = file("entry-script.sh")
+              
+  user_data_replace_on_change = false
+
   tags = {
     Name: "${var.env_prefix}-server"
   } 
