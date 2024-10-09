f668a8cc0eb6041a1c437f94324d3f32d1865c01
Tarzone <walekunle@gmail.com>
2024-10-09 00:57:46 -0600

    configure webserver modules

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,4 @@
-# Output AMI ID
-output "aws_ami_id" {
-  value = data.aws_ami.latest-amazon-linux-image.id
-}
-
 # Output ec2 Public IP
 output "ec2_public_ip" {
-  value = aws_instance.myapp-server.public_ip
+  value = module.myapp-server.instance.public_ip
 }
